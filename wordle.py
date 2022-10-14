72a09cd28d7b38fedc1768cb200aaaabcbb1dfbd
zzzhhiiiii <a0909382228@gmail.com>
2022-10-14 17:43:56 +0800

    Completed answer checking.

--- wordle.py
+++ wordle.py
@@ -2,5 +2,16 @@ if __name__ == "__main__":
     # User input
     user_input = input()
 
-    #specify an answer.
+    # Specify an answer.
     answer ="apple"
+
+    # Compare user input and answer.
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
+
+                
