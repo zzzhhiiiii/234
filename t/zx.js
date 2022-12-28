6b05fabcd78020b6f7119a7f3e12c694e15e37e7
zzzhhiiiii <a0909382228@gmail.com>
2022-12-28 17:23:44 +0800

    The prediction appears in the DevTool.

--- t/zx.js
+++ t/zx.js
@@ -1,23 +1,25 @@
 const CANVAS_SIZE = 400;
 const CANVAS_SCALE = 0.07;
 const INFERENCE_SIZE = 28;
+
+const options = { willReadFrequently: true };
 const canvas = document.getElementById("canvas"); 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d", options);
 const rect = canvas.getBoundingClientRect();
 
 const hiddenCanvas = document.getElementById("hiddenCanvas");
-const hiddenCanvasCtx = hiddenCanvas.getContext("2d");
+const hiddenCanvasCtx = hiddenCanvas.getContext("2d", options);
 hiddenCanvasCtx.scale(CANVAS_SCALE, CANVAS_SCALE);
 
+const hasTouchEvent = 'ontouchstart' in window ? true : false;
+const sess = new onnx.InferenceSession();
+const loadingModelPromise = sess.loadModel("onnx_model.onnx");
 
 let isMouseActive = false;
 let x1 = 0;
 let y1 = 0;
 let x2 = 0;
 let y2 = 0;
-
-const sess = new onnx.InferenceSession();
-const loadingModelPromise = sess.loadModel("onnx_model.onnx");
 
 function softmax(arr) {
     return arr.map(function (value, index) {
@@ -45,29 +47,18 @@ async function updatePredictions() {
     const maxPrediction = Math.max(...predictions);
     const predictLabel = predictions.findIndex((n) => n == maxPrediction);
     console.log(predictLabel);
-}
-
-
-function clearArea() {
-    // Use the identity matrix while clearing the canvas
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    hiddenCanvasCtx.setTransform(CANVAS_SCALE, 0, 0, CANVAS_SCALE, 0, 0);
-    hiddenCanvasCtx.clearRect(0, 0, hiddenCanvasCtx.canvas.width / CANVAS_SCALE, hiddenCanvasCtx.canvas.height / CANVAS_SCALE);
-}
-
-function clearBar() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < predictions.length; i++) {
         const bar = document.getElementById(`bar-${i}`);
-        bar.style.width = "0px";
-    }
-}
-
-function clearNumHighlight() {
-    for (let i = 0; i < 10; i++) {
         const num = document.getElementById(`num-${i}`);
-        num.style.color = "#000000";
-        num.style.fontWeight = "";
+        bar.style.width = `${predictions[i] * 200}px`;
+        if (predictLabel == i) {
+            bar.style.backgroundColor = "#87A2FB";
+            num.style.fontWeight = "bold";
+        }
+        else {
+            bar.style.backgroundColor = "#eeeeee";
+            num.style.fontWeight = "";
+        }
     }
 }
 
@@ -81,13 +72,13 @@ function getPos(x, y) {
 const vm = Vue.createApp({
     data () {
       return {
-        width: 10
+        width: 15
       }
     }
   }).mount('#app');
 
 let range = document.getElementById("width");
-let lWidth = 10;
+let lWidth = 15;
 range.onchange = function(){
     lWidth = this.value;
 };
@@ -102,7 +93,7 @@ function startWrite(e) {
     y1 = pos.y;
 }
 
-
+// Actual drawing.
 function Writing(e) {
 
     if (!isMouseActive) {
@@ -112,7 +103,6 @@ function Writing(e) {
     x2 = pos.x;
     y2 = pos.y;
 
-		// Actual drawing.
     ctx.beginPath(); //開始路徑
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2); //畫路徑到新的位置
@@ -128,18 +118,63 @@ function endWrite(e) {
     isMouseActive = false;
 }
 
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    hiddenCanvasCtx.setTransform(CANVAS_SCALE, 0, 0, CANVAS_SCALE, 0, 0);
+    hiddenCanvasCtx.clearRect(0, 0, hiddenCanvasCtx.canvas.width / CANVAS_SCALE, hiddenCanvasCtx.canvas.height / CANVAS_SCALE);
+}
+
+function clearBar() {
+    for (let i = 0; i < 10; i++) {
+        const bar = document.getElementById(`bar-${i}`);
+        console.log(bar);
+        bar.style.width = "0px";
+    }
+}
+
+function clearNumHighlight() {
+    for (let i = 0; i < 10; i++) {
+        const num = document.getElementById(`num-${i}`);
+        num.style.color = "#000000";
+        num.style.fontWeight = "";
+    }
+}
+
 // jQuery document ready
 loadingModelPromise.then(() => {
     
     $("#clear").click(() => {
         clearArea();
         clearBar();
-        clearNumHighlight();
+        clearNumHighlight(); 
     });
 
-    canvas.addEventListener("mousedown", startWrite);
-    canvas.addEventListener("mousemove", Writing);
-    canvas.addEventListener("mouseup", endWrite);
+    canvas.addEventListener("mouseup", updatePredictions);
+
+    if(hasTouchEvent){
+        canvas.ontouchstart = function(e){
+          startWrite(e);
+        }
+        canvas.ontouchmove = function(e){
+          Writing(e);
+        }
+        canvas.ontouchend = function(e){
+          endWrite(e);
+        }
+      } else {
+        canvas.onmousedown = function(e){
+          startWrite(e);
+        }
+        canvas.onmousemove = function(e){
+          Writing(e);
+        }
+        canvas.onmouseup = function(e){
+          endWrite(e);
+        }
+      }
+      
 });
 
 
@@ -148,5 +183,23 @@ Color.addEventListener('change', (e) => {
     ctx.strokeStyle = e.target.value
 })
 
-
+$(function(){
+    var r = $('input');
+    r.on('mouseenter',function(){
+      var p = r.val();
+      r.on('click',function(){
+        p = r.val();
+        bg(p);
+      });
+      r.on('mousemove',function(){
+        p = r.val();
+        bg(p);
+      });
+    });
+    function bg(n){
+        r.css({
+          'background-image':'-webkit-linear-gradient(left ,#f22 0%,#f22 '+n+'%,#fff '+n+'%, #fff 100%)'
+        });
+    }
+  });
 
